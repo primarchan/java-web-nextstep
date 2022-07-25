0dd8b1ea625d85aef5ba18bb8affbeef6a5a80e4
Kevin An <boltcarrier@naver.com>
2022-07-25 18:11:01 +0900

    5.1.2.1 요청 데이터를 처리하는 로직을 별도의 클래스로 분리(HttpRequest) 5.1.2.2 응답 데이터를 처리하는 로직을 별도의 클래스로 분리(HttpResponse) 5.1.2.3 다형성을 활용해 클라이언트 요청을 URL 에 대한 분기 처리 제거

---
+++ src/test/java/http/HttpRequestTest.java
@@ -0,0 +1,35 @@
+package http;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class HttpRequestTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals(HttpMethod.GET, request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("primarchan", request.getParameter("userId"));
+    }
+
+    @Test
+    public void request_POST() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals(HttpMethod.POST, request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("primarchan", request.getParameter("userId"));
+    }
+}
---
+++ src/test/java/http/HttpResponseTest.java
@@ -0,0 +1,38 @@
+package http;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class HttpResponseTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void responseForward() throws Exception {
+        // Http_forward.txt 결과는 응답 body 에 index.html 파일이 포함되어 있어야 함
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
+        response.forward("/index.html");
+    }
+
+    @Test
+    public void responseRedirect() throws Exception {
+        // Http_Cookie.txt 결과는 응답 header 에 Set-Cookie 값으로
+        // logined=true 값이 포함되어 있어야 함
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
+        response.sendRedirect("/index.html");
+    }
+
+    @Test
+    public void responseCookies() throws Exception {
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
+        response.addHeader("Set-Cookie", "logined=true");
+        response.sendRedirect("/index.html");
+    }
+
+    private OutputStream createOutputStream(String filename) throws FileNotFoundException {
+        return new FileOutputStream(new File(testDirectory + filename));
+    }
+}
